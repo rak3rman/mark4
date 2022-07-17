5a2262d33fc9862245385a6e8b1238f8781b2af5
Radison Akerman <rak3rman@gmail.com>
2022-07-16 20:38:55 -0500

    Experience section switcher updates, testimonials template

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
               <div class="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span class="sr-only">RAkerman</span>
-                  <img class="h-5 w-auto sm:h-6" src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/7ed7598b-5520-4e18-5159-daa458739b00/sm" alt="" />
+                  <img class="h-5 w-auto sm:h-6" src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/7ed7598b-5520-4e18-5159-daa458739b00/sm" alt="RAkerman Logo" />
                 </a>
                 <div class="-mr-2 flex items-center md:hidden">
                   <PopoverButton class="bg-primary-focus rounded-md p-2 inline-flex items-center justify-center text-accent focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary">
@@ -37,7 +37,7 @@
             <div class="rounded-lg shadow-md bg-primary-focus ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div class="px-5 pt-4 flex items-center justify-between">
                 <div>
-                  <img class="h-5 w-auto" src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/7ed7598b-5520-4e18-5159-daa458739b00/sm" alt="" />
+                  <img class="h-5 w-auto" src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/7ed7598b-5520-4e18-5159-daa458739b00/sm" alt="RAkerman Logo" />
                 </div>
                 <div class="-mr-2">
                   <PopoverButton class="bg-primary rounded-md p-2 inline-flex items-center justify-center text-accent hover:bg-primary-focus focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary">
@@ -83,7 +83,7 @@
               <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
                 <figure>
                   <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
-                    <img class="rounded-lg shadow-lg object-cover object-top" src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/60165710-ba66-465f-f366-ba1c3b73cd00/xl" alt="Portrait of Radison" width="1184" height="1376" />
+                    <img class="rounded-lg shadow-lg object-cover object-top" src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/60165710-ba66-465f-f366-ba1c3b73cd00/xl" alt="Portrait of Radison" />
                   </div>
                 </figure>
               </div>
@@ -113,14 +113,22 @@
                 <option v-for="tab in expTabs" :value="tab.id" :selected="tab.id === selectedExpTab">{{ tab.name }}</option>
               </select>
             </div>
-            <div class="hidden sm:block">
-              <div class="border-b border-gray-200">
-                <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-                  <a v-for="tab in expTabs" href="#experience" :key="tab.id" @click="updateExpTab(tab.id)" :class="[tab.id === selectedExpTab ? 'border-secondary text-secondary' : 'border-transparent text-accent hover:text-secondary hover:border-accent', 'group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm']" :aria-current="tab.id === selectedExpTab ? 'page' : undefined">
-                    <component :is="tab.icon" :class="[tab.id === selectedExpTab ? 'text-secondary' : 'text-accent group-hover:text-secondary', '-ml-0.5 mr-2 h-5 w-5']" aria-hidden="true" />
-                    <span>{{ tab.name }}</span>
-                  </a>
-                </nav>
+            <div class="hidden sm:block sm:mt-4 sm:mb-3">
+              <div class="grid grid-cols-3 gap-x-8" role="tablist" aria-orientation="horizontal">
+                <div class="relative transition ease-in-out duration-300" v-for="tab in expTabs" href="#experience" :key="tab.id" @click="updateExpTab(tab.id)" :class="[tab.id === selectedExpTab ? '' : 'opacity-75 hover:opacity-100']">
+                  <div class="w-9 rounded-lg transition ease-in-out duration-300" :class="[tab.id === selectedExpTab ? 'bg-secondary' : 'bg-primary-focus']">
+                    <svg aria-hidden="true" class="h-9 w-9 p-2" fill="#fff" :viewBox="tab.viewbox" v-html="tab.icon"></svg>
+                  </div>
+                  <h3 class="mt-6 text-md font-medium transition ease-in-out duration-300" :class="[tab.id === selectedExpTab ? 'text-secondary' : 'text-neutral']">
+                    <button>
+                      <span class="absolute inset-0"></span>
+                      {{ tab.name }}
+                    </button>
+                  </h3>
+                  <p class="mt-2 font-display text-md text-accent">
+                    {{ tab.desc }}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -142,11 +150,37 @@
             </div>
           </div>
 
+          <ul role="list" class="mt-16 grid grid-cols-1 gap-8 lg:grid-cols-3">
+            <li v-for="(group, index) in testimonials" class="space-y-8">
+              <figure class="rounded-xl p-8 shadow-md ring-1 ring-primary-focus" v-for="testimonial in group" :class="index < 1 ? 'hidden lg:block' : ''">
+                <blockquote>
+                  <p class="text-lg tracking-tight text-neutral">
+                    "{{ testimonial.quote }}"
+                  </p>
+                </blockquote>
+                <figcaption class="mt-6 flex items-center">
+                  <div class="overflow-hidden rounded-full bg-secondary">
+                    <img class="h-12 w-12 object-cover" :src="'https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/' + testimonial.img + '/sq'" alt="Image" />
+                  </div>
+                  <div class="ml-4">
+                    <div class="text-base font-medium leading-6 tracking-tight text-neutral">
+                      {{ testimonial.name }}
+                    </div>
+                    <div class="mt-1 text-sm text-accent">
+                      {{testimonial.title }}
+                    </div>
+                  </div>
+                </figcaption>
+              </figure>
+            </li>
+          </ul>
+
           <!--  03. Projects  -->
           <HeaderLine num="03." title="Projects" id="projects" />
 
           <!--  04. Contact  -->
           <HeaderLine num="04." title="Get In Touch" id="contact" />
+
 
           <!--  Footer  -->
           <div class="w-full pt-16 text-sm text-center md:text-left fade-in">
@@ -175,6 +209,21 @@ const navigation = [
   { name: 'Projects', href: '#projects' },
   { name: 'Contact', href: '#contact' },
 ]
+
+const testimonials = [
+  [
+    { name: 'Radison Akerman', title: 'Front-end Developer', quote: "Mira’s teaching style is second to none. Everything was easy to follow every step of the way.", img: "60165710-ba66-465f-f366-ba1c3b73cd00" },
+    { name: 'Radison Akerman', title: 'Front-end Developer', quote: "I run an ecommerce store selling rare vintage gummy bears and could never find a good gummy bear icon. Now I can design my own in minutes.", img: "60165710-ba66-465f-f366-ba1c3b73cd00" },
+  ],
+  [
+    { name: 'Radison Akerman', title: 'Front-end Developer', quote: "I couldn’t believe how fast Mira moved in Figma compared to my own workflow. I’m designing icons more accurately in half the time with the shortcuts I learned from her videos.", img: "60165710-ba66-465f-f366-ba1c3b73cd00" },
+    { name: 'Radison Akerman', title: 'Front-end Developer', quote: "Even though I was excited to learn, I was pessimistic that I wouldn’t actually ever get good enough to design my own icons. I was wrong — this book is all I needed.", img: "60165710-ba66-465f-f366-ba1c3b73cd00" },
+  ],
+  [
+    { name: 'Radison Akerman', title: 'Front-end Developer', quote: "The complete package is worth it for the weekly teardown videos alone. I’ve learned so much watching Mira take apart other icons and recreate them from scratch.", img: "60165710-ba66-465f-f366-ba1c3b73cd00" },
+    { name: 'Radison Akerman', title: 'Front-end Developer', quote: "I never thought I would enjoy designing icons but using the ideas in this book, it’s become a great way for me to relax while still being creative.", img: "60165710-ba66-465f-f366-ba1c3b73cd00" },
+  ],
+]
 </script>
 
 <script>
@@ -182,9 +231,27 @@ import {CameraIcon, DesktopComputerIcon, UserGroupIcon} from "@heroicons/vue/sol
 
 const selected_exp_tab = 'cs'
 const exp_tabs = [
-  { id: 'cs', name: 'Computer Science', icon: DesktopComputerIcon },
-  { id: 'lead', name: 'Leadership', icon: UserGroupIcon },
-  { id: 'photo', name: 'Photography', icon: CameraIcon},
+  {
+    id: 'cs',
+    name: 'Computer Science',
+    desc: 'Pushing the boundaries of full-stack web development, dev-ops, and cybersecurity.',
+    icon: '<path d="M160 80C160 112.8 140.3 140.1 112 153.3V241.1C130.8 230.2 152.7 224 176 224H272C307.3 224 336 195.3 336 160V153.3C307.7 140.1 288 112.8 288 80C288 35.82 323.8 0 368 0C412.2 0 448 35.82 448 80C448 112.8 428.3 140.1 400 153.3V160C400 230.7 342.7 288 272 288H176C140.7 288 112 316.7 112 352V358.7C140.3 371 160 399.2 160 432C160 476.2 124.2 512 80 512C35.82 512 0 476.2 0 432C0 399.2 19.75 371 48 358.7V153.3C19.75 140.1 0 112.8 0 80C0 35.82 35.82 0 80 0C124.2 0 160 35.82 160 80V80zM80 104C93.25 104 104 93.25 104 80C104 66.75 93.25 56 80 56C66.75 56 56 66.75 56 80C56 93.25 66.75 104 80 104zM368 56C354.7 56 344 66.75 344 80C344 93.25 354.7 104 368 104C381.3 104 392 93.25 392 80C392 66.75 381.3 56 368 56zM80 456C93.25 456 104 445.3 104 432C104 418.7 93.25 408 80 408C66.75 408 56 418.7 56 432C56 445.3 66.75 456 80 456z"/>',
+    viewbox: '0 0 448 512',
+  },
+  {
+    id: 'lead',
+    name: 'Leadership',
+    desc: 'Effectively leading teams of all sizes over a wide gamut of industries.',
+    icon: '<path d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3c-95.73 0-173.3 77.6-173.3 173.3C0 496.5 15.52 512 34.66 512H413.3C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM479.1 320h-73.85C451.2 357.7 480 414.1 480 477.3C480 490.1 476.2 501.9 470 512h138C625.7 512 640 497.6 640 479.1C640 391.6 568.4 320 479.1 320zM432 256C493.9 256 544 205.9 544 144S493.9 32 432 32c-25.11 0-48.04 8.555-66.72 22.51C376.8 76.63 384 101.4 384 128c0 35.52-11.93 68.14-31.59 94.71C372.7 243.2 400.8 256 432 256z"/>',
+    viewbox: '0 0 640 512',
+  },
+  {
+    id: 'photo',
+    name: 'Photography',
+    desc: 'Specializing in stills photography with clients all over the world.',
+    icon: '<path d="M194.6 32H317.4C338.1 32 356.4 45.22 362.9 64.82L373.3 96H448C483.3 96 512 124.7 512 160V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V160C0 124.7 28.65 96 64 96H138.7L149.1 64.82C155.6 45.22 173.9 32 194.6 32H194.6zM256 384C309 384 352 341 352 288C352 234.1 309 192 256 192C202.1 192 160 234.1 160 288C160 341 202.1 384 256 384z"/>',
+    viewbox: '0 0 512 512',
+  },
 ]
 const exp_entries = [
   {
