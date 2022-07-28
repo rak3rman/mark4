13522201cdc855356130317afd0550e7292082dd
Radison Akerman <rak3rman@gmail.com>
2022-07-28 14:17:56 -0500

    Nuxt and Wrangler = head scratches

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,8 @@ export default defineNuxtConfig({
     },
     build: {
         transpile: [
-            '@headlessui/vue'
+            '@headlessui/vue',
+            '@heroicons/vue'
         ]
     }
 })
