814e4092ec0d7d68095086703f9f7d47a28f22a9
Radison Akerman <rak3rman@gmail.com>
2022-08-05 01:01:22 -0500

    Meta default

--- app.vue
+++ app.vue
@@ -1,10 +1,13 @@
 <template>
+  <Title>Radison Akerman</Title>
   <Html class="h-full bg-primary scroll-smooth" lang="en" />
   <Meta charset="UTF-8" />
   <Meta name="HandheldFriendly" content="True" />
   <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <Meta name="referrer" content="no-referrer-when-downgrade" />
   <Link rel="icon" type="png" href="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/cbeec7ce-7b51-4cc2-81bb-e72289777900/sm" />
+  <Meta property="og:image" content="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/fa35b458-67ec-4711-0256-9f68535cbd00/md" />
+  <Meta name="description" content="I am highly skilled in internet. You might find me solving niche problems with programming, over yonder with my camera, or working on something radical." />
   <Body class="h-full bg-primary" />
   <div class="h-full bg-primary">
     <NuxtPage />
