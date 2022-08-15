a4710f7c2709fec067ea9605a16c204f838d20a3
Radison Akerman <rak3rman@gmail.com>
2022-08-15 15:14:23 -0500

    Updated project archive formatting

--- components/projects/Archive.vue
+++ components/projects/Archive.vue
@@ -7,7 +7,7 @@
           <tr class="text-accent">
             <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-6 md:pl-0">Period</th>
             <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold">Title</th>
-            <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold">Made at</th>
+            <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold hidden md:table-cell">Made at</th>
             <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold">Built with</th>
             <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold">Link</th>
           </tr>
@@ -15,8 +15,8 @@
           <tbody class="divide-y divide-accent/20">
           <tr v-for="project in filtered" :key="project.title" class="">
             <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-secondary sm:pl-6 md:pl-0">{{ DateTime.fromMillis(Date.parse(project.start)).toFormat('LLL yyyy') }}</td>
-            <td class="whitespace-nowrap py-4 px-3 text-sm text-neutral">{{ project.title }}</td>
-            <td class="whitespace-nowrap py-4 px-3 text-sm text-accent">{{ project.made_at }}</td>
+            <td class="whitespace-nowrap py-4 px-3 text-sm text-neutral">{{ project.title }} <span class="table-cell md:hidden text-accent font-light">@ {{ project.made_at }}</span></td>
+            <td class="whitespace-nowrap py-4 px-3 text-sm text-accent hidden md:table-cell">{{ project.made_at }}</td>
             <td class="whitespace-nowrap py-4 px-3 text-xs text-accent font-mono text-accent font-light"><div class="flex flex-wrap"><h6 v-for="tool in project.tools" :class="'pr-4'">{{ tool }}</h6></div></td>
             <td class="whitespace-nowrap py-4 px-3 text-sm text-accent"><div class="flex items-center"><ProjectsExtIcons :project="project" /></div></td>
           </tr>
