463e17aaf4986d3a83b7c3568fb671ef6bcb8b04
Radison Akerman <rak3rman@gmail.com>
2023-01-23 17:17:52 -0600

    About section image updates

--- components/home/About.vue
+++ components/home/About.vue
@@ -2,13 +2,28 @@
     <div>
         <Header num="01." title="About Me" id="about" fade/>
         <div class="mt-4">
-            <div class="relative lg:float-right lg:pl-5 lg:pb-5 lg:pt-1 fade-in">
+            <div class="relative lg:float-right lg:pl-5 lg:pb-5 lg:pt-1">
                 <div class="relative text-base mx-auto max-w-prose lg:max-w-md">
-                    <figure>
-                        <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
+                    <figure class="lg:w-[40rem] lg:h-[40rem]">
+                        <div class="lg:hidden aspect-w-12 aspect-h-7 lg:aspect-none fade-in">
                             <img class="rounded-lg shadow-lg object-cover object-top"
                                  src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/60165710-ba66-465f-f366-ba1c3b73cd00/xl"
                                  alt="Portrait of Radison"/>
+                        </div>
+						<div class="hidden lg:block ml-[2rem] fade-in">
+                            <img class="rounded-lg shadow-lg object-cover object-top w-[19rem]"
+                                 src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/60165710-ba66-465f-f366-ba1c3b73cd00/xl"
+                                 alt="Portrait of Radison"/>
+                        </div>
+						<div class="hidden lg:block absolute top-0 left-0 mt-[26rem] ml-[0rem] overflow-hidden rounded-lg shadow-xl fade-in">
+                            <img class="object-cover object-top w-[16rem] h-[15rem]"
+                                 src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/b6b21ae7-e308-443c-a354-976ae0a02300/2xl"
+                                 alt="Radison Lecturing at UIC"/>
+						 </div>
+						<div class="hidden lg:block absolute top-0 left-0 mt-[13rem] ml-[12rem] overflow-hidden rounded-lg shadow-xl fade-in">
+                            <img class="object-cover object-top scale-[135%] -translate-x-2 w-[18rem]"
+                                 src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/b6cd4c78-5828-4edf-7ce0-603681a5cf00/2xl"
+                                 alt="Radison Biking"/>
                         </div>
                     </figure>
                 </div>
@@ -33,12 +48,12 @@
                         <NuxtLink class="text-secondary underline underline-offset-2" to="https://uic.edu">University of Illinois Chicago</NuxtLink>.
 						For starters, I am studying computer science and business full-time (with a 4.0 GPA, whoop!).
                         I also project manage 7 web and database developers working on over 10 full-stack web
-                        applications for the College of Engineering, part-time.
+                        applications for the College of Engineering.
                     </p>
                     <p class="fade-in">
 						Making an impact is part of my DNA, especially when it comes to personal
                         projects. I recently gave
-                        <NuxtLink class="text-secondary underline underline-offset-2" to="https://youtu.be/cD53M7NHQoM">a 300-level guest lecture</NuxtLink> (twice) at UIC about
+                        <NuxtLink class="text-secondary underline underline-offset-2" to="https://youtu.be/cD53M7NHQoM">two separate 300-level guest lectures</NuxtLink> at UIC about
                         <NuxtLink class="text-secondary underline underline-offset-2" to="https://chickens.rakerman.com">Exploding Chickens</NuxtLink> —
 						a popular full-stack card game I built with my friends. We dove deep into project management,
                         design patterns, code review, and test coverage.
@@ -49,6 +64,10 @@
                         Foundation answered this call and now hosts over
                         <NuxtLink class="text-secondary underline underline-offset-2" to="https://status.rakerman.com">10 web services</NuxtLink>,
 						2 web products generating revenue, and 5 organizational members.
+                    </p>
+					<p class="fade-in">
+						In my spare time, I love to building things with my hands (woodworking, small electronics) and competing in endurance triathlons.
+						Just this past summer, I finished <NuxtLink class="text-secondary underline underline-offset-2" to="https://www.athlinks.com/event/30585/results/Event/1019369/Course/2251422/Bib/3435">214th out of 2,456</NuxtLink> in the Chicago Triathlon, Olympic distance &mdash; my first ever multisport race.
                     </p>
                 </div>
             </div>
