ac7d036ed632c39a93555eda617a3c757521cce0
Radison Akerman <rak3rman@gmail.com>
2023-04-08 13:22:09 -0500

    bug: build issue with icons

--- components/projects/Quickview.vue
+++ components/projects/Quickview.vue
@@ -104,14 +104,10 @@
 import {
     Dialog,
     DialogPanel,
-    RadioGroup,
-    RadioGroupLabel,
-    RadioGroupOption,
     TransitionChild,
     TransitionRoot,
 } from '@headlessui/vue'
 import { XMarkIcon } from '@heroicons/vue/24/outline'
-import { StarIcon } from '@heroicons/vue/20/solid'
 import {
     ComputerDesktopIcon,
     CpuChipIcon,
@@ -120,26 +116,8 @@ import {
     TagIcon, WrenchIcon,
     CalendarDaysIcon,
     AtSymbolIcon,
-} from "@heroicons/vue/24/outline/index.js";
+} from '@heroicons/vue/24/outline'
 import {DateTime} from 'luxon'
-
-const product = {
-    name: 'Zip Tote Basket',
-    price: '$220',
-    rating: 3.9,
-    href: '#',
-    description:
-        'The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-03-product-04.jpg',
-    imageAlt: 'Back angled view with bag open and handles to the side.',
-    colors: [
-        { name: 'Washed Black', bgColor: 'bg-gray-700', selectedColor: 'ring-gray-700' },
-        { name: 'White', bgColor: 'bg-white', selectedColor: 'ring-gray-400' },
-        { name: 'Washed Gray', bgColor: 'bg-gray-500', selectedColor: 'ring-gray-500' },
-    ],
-}
-
-const selectedColor = ref(product.colors[0])
 
 const props = defineProps({
     show: Boolean,
