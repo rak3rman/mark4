4cf8789b8eed621cfbad82127dc74e77bb0be8ab
Radison Akerman <radison@me.com>
2024-04-13 17:22:04 -0500

    feat: added prose components, .gitignore

---
+++ nuxt/components/content/ProseH2.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2 class="pb-4 text-2xl font-medium">
+    <slot />
+  </h2>
+</template>
---
+++ nuxt/components/content/ProseH4.vue
@@ -0,0 +1,5 @@
+<template>
+  <h4 class="pb-2 text-xl font-medium">
+    <slot />
+  </h4>
+</template>
