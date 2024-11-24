230bf46dfb75aca29e18a4fe7bab0e7c4304c5a1
Radison Akerman <radison@me.com>
2024-11-24 12:57:35 -0800

    nit: headshot padding

--- nuxt/components/content/HomeAbout.vue
+++ nuxt/components/content/HomeAbout.vue
@@ -7,12 +7,12 @@
         <div class="relative mx-auto max-w-prose text-base lg:max-w-md">
           <figure class="lg:h-[40rem] lg:w-[28rem]">
             <div
-              class="fade-in aspect-h-7 aspect-w-12 lg:aspect-none lg:hidden"
+              class="fade-in aspect-h-7 aspect-w-12 lg:aspect-none lg:hidden overflow-hidden"
             >
               <NuxtImg
-                class="object-cover object-center shadow-lg bg-base-200 [mask-image:linear-gradient(to_bottom,black_70%,transparent_95%)] scale-[118%] sm:scale-[100%]"
+                class="object-cover object-center shadow-lg bg-base-200 [mask-image:linear-gradient(to_bottom,black_70%,transparent_95%)] scale-[117%] sm:scale-[100%]"
                 src="https://imagedelivery.net/5zM6Rdl2uV8Hmr9WxRh20g/af63a776-af55-4f19-22a7-604a95399b00/xl"
-                alt="Portrait of Radison"
+                alt="Black BackgroundPortrait of Radison"
               />
             </div>
             <div class="fade-in ml-[2rem] hidden lg:block">
